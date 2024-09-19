bf7f0ec94118e4a8a6415d5d5ffb31a3d04ec803
kornchetchen <inpicblog@gmail.com>
2024-09-20 00:40:35 +0700

    update

--- main.go
+++ main.go
@@ -25,8 +25,23 @@ type Director {
 
 var movie []Movie
 
+fubc getMovie( w http.ResponseWritter. r *http.Request)
+	w.Header().Set("Content-Type ". "application/json"
+	json.NewEncoder(w).Encode(movie)
+)
+
+
 func main(){
 	r :=mux.NewRouter() //function in side mux 
 
-	r.HandleFunc 
+	movie = append(movie , Movie{ID: "1", Isbn:"438227", Title: "Movie one" , Director : &Director{Firstname:" John ", Lastname:"Doe"}})
+	movie = append (movies, Movie{ ID:"2", Isbn: "45455",Title: "Movie Two" , Director: &Director{Firstname:"Steve",Lastname:"smith"}})
+	r.HandleFunc("/movie",getMovie).methods("GET")
+	r.HandleFunc("/movies/{id}",getMovie).Methods("GET")
+	r.HandleFunc("/movies/{id}",createMovie).Methods("POST")
+	r.HandleFunc("/movies/{id}",updateMovie).Methods("PUT")
+	r.HandleFunc("/movies/{id}",deleteMovie).Methods("DELETE")
+
+	fmt.Print("Starting server at port 8080\n")
+	log.Fatal(http.ListenAndsrve(":8000",r))
 }
